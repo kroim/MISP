b09fa4a93487ffd21e781c4b7828e492018a0d5d
kroim <wonderfulplayer@outlook.com>
2021-02-02 11:42:50 +0800

    remove old

--- app/View/Elements/formInfo.ctp
+++
@@ -1,1 +0,0 @@
-<span id = "<?php echo $type?>InfoPopover" class="icon-info-sign" data-toggle="popover" data-field="<?php echo $type; ?>"></span>
--- app/files/scripts/mispzmq/mispzmqtest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-try:
-    import zmq
-except ImportError:
-    print("ZeroMQ library could not be imported.")
-print("OK")
