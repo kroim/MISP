94d5ca442a43a99946a9df0ceda211082f99a7de
kroim <wonderfulplayer@outlook.com>
2021-02-02 11:44:25 +0800

    Remove old codes

--- app/Vendor/kamisama/resque-status/tests/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-    include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'ResqueStatus' . DIRECTORY_SEPARATOR . 'ResqueStatus.php';
--- app/View/Events/json/view.ctp
+++
@@ -1,4 +0,0 @@
-<?php
-App::uses('JSONConverterTool', 'Tools');
-$converter = new JSONConverterTool();
-echo $converter->convert($event);
